bbf66949a8d46ca65b25b7c279db17ea823abb69
Liren Wei <lrwei@bupt.edu.cn>
2021-09-20 12:35:09 +0800

    Exercise of Lab1

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -179,7 +179,10 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	//	Look at the STABS documentation and <inc/stab.h> to find
 	//	which one.
 	// Your code here.
-
+	stab_binsearch(stabs, &lline, &rline, N_SLINE, addr);
+	if (lline > rline)
+		return -1;
+	info->eip_line = stabs[lline].n_desc;
 
 	// Search backwards from the line number for the relevant filename
 	// stab.
@@ -197,10 +200,9 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	// Set eip_fn_narg to the number of arguments taken by the function,
 	// or 0 if there was no containing function.
 	if (lfun < rfun)
-		for (lline = lfun + 1;
-		     lline < rfun && stabs[lline].n_type == N_PSYM;
-		     lline++)
-			info->eip_fn_narg++;
+		for (lline = lfun + 1; lline < rfun; lline++)
+			if (stabs[lline].n_type == N_PSYM)
+				info->eip_fn_narg++;
 
 	return 0;
 }
